b4c4e481a59bf6a9df4baec4d052ee2457053b00
Matthew Landauer <matthew@openaustralia.org>
2014-01-21 17:52:08 +1100

    Following instructions to get library to work

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,5 @@
 require 'scraperwiki'
-# TODO:
-# 1. Fork the ScraperWiki library (if you haven't already) at https://classic.scraperwiki.com/scrapers/icon_rest_xml/
-# 2. Add the forked repo as a git submodule in this repo
-# 3. Change the line below to something like require File.dirname(__FILE__) + '/icon_rest_xml/scraper'
-# 4. Remove these instructions
+require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 require 'scrapers/icon_rest_xml'
 
 scrape_icon_rest_xml("http://aps.gtcc.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
