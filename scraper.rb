0026fcf12c2a8d5d22943f178e8b23d647bb3c98
Matthew Landauer <matthew@openaustralia.org>
2014-01-21 17:52:47 +1100

    Oops. left the old require in there

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,5 @@
 require 'scraperwiki'
 require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
-require 'scrapers/icon_rest_xml'
 
 scrape_icon_rest_xml("http://aps.gtcc.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
 
