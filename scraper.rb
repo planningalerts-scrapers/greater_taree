d7872fe0466f249d82597e70644a76312546728b
ishmagel <ishmagel@gmail.com>
2015-06-17 14:42:49 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,4 @@
 require 'scraperwiki'
 require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
-scrape_icon_rest_xml("http://icon.gtcc.nsw.gov.au/eplanning/Pages/XC.Track/SearchApplication.aspx?d=last14days&k=LodgementDate&t=290&o=xml")
+scrape_icon_rest_xml("http://icon.gtcc.nsw.gov.au/eplanning/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
