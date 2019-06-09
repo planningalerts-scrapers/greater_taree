8dee618a86a68e8aafd7aabce84653b5b98c73c6
Matthew Landauer <mlandauer@gmail.com>
2019-06-09 18:07:06 +1000

    Use more of the library

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,9 @@
 require "icon_scraper"
 
-IconScraper.rest_xml("http://icon.gtcc.nsw.gov.au/eplanning/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
+IconScraper.scrape_with_params(
+  url: "http://icon.gtcc.nsw.gov.au/eplanning",
+  period: "last14days",
+  types: [290]
+) do |record|
+  IconScraper.save(record)
+end
